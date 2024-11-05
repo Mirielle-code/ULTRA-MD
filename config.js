86cdba03c694930bfd999305dac6cb02d0315edf
Mirielle-code <miriellesong@yahoo.com>
2024-11-04 20:41:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -73,7 +73,7 @@ global.APIKeys = {
 // Sticker WM
 global.premium = 'true'
 global.packname = 'GLOBAL-MD'
-global.author = 'GlobalTechInfo'
+global.author = 'Snake-code'
 global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
 global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/global.techinfo'
 global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
